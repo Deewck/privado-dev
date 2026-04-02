e8c733b4bafdcd58f8c3b63fc8d829c2e454a1e4
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-02 13:34:29 -0600

    Fase1: Estructura de funcionamiento y conexion Db

---
+++ BE-asistencia/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import sesionesRoutes from './routes/sesiones.js'
+import supabase from './services/supabaseClient.js'
+
+const app = express()
+app.use(express.json())
+
+app.use(sesionesRoutes)
+
+app.get('/test', async (req, res) => {
+  const { data, error } = await supabase
+    .from('roles')
+    .select('*')
+
+  if (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+
+  res.json(data)
+})
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en puerto 3000')
+})
---
+++ BE-asistencia/routes/sesiones.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import supabase from '../services/supabaseClient.js'
+
+const router = express.Router()
+
+router.post('/sesiones', async (req, res) => {
+  try {
+    const { idCurso } = req.body
+
+    if (!idCurso) {
+      return res.status(400).json({ error: 'idCurso es requerido' })
+    }
+
+    const token = Math.random().toString(36).substring(2, 10)
+    const ahora = new Date()
+    const expira = new Date(Date.now() + 60000) // 1 minuto
+
+    const { data, error } = await supabase
+      .from('sesiones')
+      .insert([
+        {
+          idcurso: idCurso,
+          token: token,
+          activa: true,
+          fechainicio: ahora,
+          expiraen: expira
+        }
+      ])
+      .select()
+
+    if (error) {
+      console.log(error)
+      return res.status(500).json({ error })
+    }
+
+    res.json({
+      mensaje: 'Sesión creada',
+      token: token,
+      expiraEn: expira
+    })
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'Error interno' })
+  }
+})
+
+export default router
