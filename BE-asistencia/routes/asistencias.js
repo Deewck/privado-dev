d6b3a4f4574f412368333f59fc87d33b530a696d
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-05 00:42:57 -0600

    Rf

--- BE-asistencia/routes/asistencias.js
+++ BE-asistencia/routes/asistencias.js
@@ -1,7 +1,6 @@
-import express              from 'express'
-import supabase             from '../services/supabaseClient.js'
-import {validarRol}         from '../middleware/validarRol.js'
-import {authMiddleware}     from '../middleware/authMiddleware.js'
+import express                        from 'express'
+import supabase                       from '../services/supabaseClient.js'
+import { authMiddleware, validarRol } from '../middleware'
 
 const router = express.Router()
 
