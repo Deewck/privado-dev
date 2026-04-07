66c22116e5709ca9c556b620083d568ebf665cbe
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-06 22:03:46 -0600

    tod

--- BE-asistencia/services/getCursosEstudiante.js
+++ BE-asistencia/services/getCursosEstudiante.js
@@ -1,23 +1,33 @@
-import supabase from './supabaseClient.js'
+import supabase from '../services/supabaseClient.js'
 
 export const obtenerInscripciones = async (user, estado) => {
   const idEstudiante = user.idUsuario
   if (estado === 'asignados') {
     const { data, error } = await supabase
       .from('ciclos')
-      .select('codigo, descripcion, cursos (codigo, descripcion, inscripciones(idEstudiante))')
-      .eq('inscripciones.idEstudiante', idEstudiante)
+      .select(`
+        codigo,
+        descripcion,
+        cursos (
+          codigo,
+          descripcion,
+          inscripciones (idEstudiante)
+        )
+      `)
+      .eq('cursos.inscripciones.idEstudiante', idEstudiante)
     if (error) throw error
-    const inscripcionesFiltradas = data.filter(c => c.inscripciones.length > 0)
-    return inscripcionesFiltradas
+    const resultado = data
+      .map(ciclo => ({
+        codigo: ciclo.codigo,
+        descripcion: ciclo.descripcion,
+        cursos: ciclo.cursos
+          .filter(curso => curso.inscripciones.length > 0)
+          .map(curso => ({
+            codigo: curso.codigo,
+            descripcion: curso.descripcion
+          }))
+      }))
+      .filter(ciclo => ciclo.cursos.length > 0)
+    return resultado
   }
-//   if (estado === 'no_asignados') {
-//     const { data, error } = await supabase
-//       .from('ciclos')
-//       .select('codigo, descripcion, cursos (codigo, descripcion)')
-//       .is('cursos.idCatedratico', null)
-//     if (error) throw error
-//     const inscripcionesFiltradas = data.filter(c => c.inscripciones.length > 0)
-//     return inscripcionesFiltradas
-//   }
 }
