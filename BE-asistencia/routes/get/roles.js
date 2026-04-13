6d60ce38863fa0c6f66f32ba104061c8c7e93a4c
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-12 19:49:59 -0600

    Roles globales

--- BE-asistencia/routes/get/roles.js
+++ BE-asistencia/routes/get/roles.js
@@ -1,11 +1,10 @@
 import express               from 'express'
-import { authMiddleware }    from '../../middleware/authMiddleware.js'
 import { obtenerRol }        from '../../services/getRol.js'
 
 const router = express.Router()
 
-router.get('/roles', authMiddleware, async (req, res) => {
-  const data = await obtenerRol(req.user)
+router.get('/roles', async (req, res) => {
+  const data = await obtenerRol(req.query.codigo)
   res.json(data)
 })
 export default router
