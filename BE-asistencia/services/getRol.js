3cefd2eda6ba07edb80ef0e4937cf88d4f664e31
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-12 19:50:58 -0600

    roles globales

--- BE-asistencia/services/getRol.js
+++ BE-asistencia/services/getRol.js
@@ -1,10 +1,19 @@
 import supabase from '../services/supabaseClient.js'
 
-export const obtenerRol = async (user) => {
-  const idUser = user.idUsuario
-  const { data, error } = await supabase
+export const obtenerRol = async (codigo) => {
+  if (!codigo) {
+    const { data, error } = await supabase
     .from('roles')
-    .select('codigo, descripcion')
-  if (error) throw error
-  return data
+    .select('codigo, descripcion, idRol')
+    if (error) throw error
+    return data
+  } else {
+    const { data, error } = await supabase
+    .from('roles')
+    .select('codigo, descripcion, idRol')
+    .eq('codigo', codigo)
+    if (error) throw error
+    console.log(codigo)
+    return data
+  }
 }
