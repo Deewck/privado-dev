366a19df6b4d837a5ad56069cef3cd63f192c27c
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-13 22:17:51 -0600

    separación de cns

--- FE-asistencia/js/ui.js
+++ FE-asistencia/js/ui.js
@@ -1,6 +1,6 @@
 import { fetchAPI } from './api.js'
 //Funcion para Cambo box de facultades
-async function cargarFacultades() {
+export async function cargarFacultades() {
   try {
     const data = await fetchAPI('/consultas/facultades')
     console.log('FACULTADES:', data)
@@ -17,7 +17,7 @@ async function cargarFacultades() {
   }
 }
 //Funcion para Cambo box de ciclos
-async function cargarCiclos() {
+export async function cargarCiclos() {
   try {
     const facultad = document.getElementById('facultadSelect').value
     const data = await fetchAPI(`/consultas/ciclos?facultad=${facultad}`)
@@ -36,7 +36,7 @@ async function cargarCiclos() {
   }
 }
 //Funcion para Cursos Con/sin filtro de ciclos
-async function cargarCursos() {
+export async function cargarCursos() {
   let data
   try {
     const facultad = document.getElementById('facultadSelect').value
@@ -65,8 +65,82 @@ async function cargarCursos() {
     console.error(err)
   }
 }
-window.addEventListener('DOMContentLoaded', () => {
-  cargarFacultades()
-  document.getElementById('facultadSelect').addEventListener('change', cargarCiclos) 
-  document.getElementById('botonBusca').addEventListener('click', cargarCursos)
-})
+export async function asignadosSN() {
+  try {
+    const radio = document.getElementById('asignado')
+    radio.addEventListener('click', function () {
+      if (this.checked && this.dataset.checked === 'true') {
+        this.checked = false
+        this.dataset.checked = 'false'
+      } else {
+        this.dataset.checked = 'true'
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+export async function misCursosEstudiante() {
+  let data
+  try {
+    const radio = document.getElementById('asignado')
+    let asignado = null
+    let estado = null
+    let corr = 1
+    if (radio.checked) {
+      asignado = radio.value
+    }
+    if (asignado) {
+      estado = 'Asignado'
+      data = await fetchAPI(`/consultas/cursoInscripcion?estado=${asignado}`)
+    } else {
+      asignado = 'no_asignados'
+      estado = 'Asignar'
+      data = await fetchAPI(`/consultas/cursoInscripcion?estado=${asignado}`)
+    }
+    console.log('Mis-Cursos:', data)
+    const select = document.getElementById('listaMisCursos')
+    select.innerHTML = `
+      <div class="list-group-item fw-bold">
+        <div class="row text-center">
+          <div class="col-1">Ciclo</div>
+          <div class="col-1">Código</div>
+          <div class="col-4 text-start">Descripción</div>
+          <div class="col-6 text-start">Estado</div>
+        </div>
+      </div>
+    `
+    data.forEach(ciclo => {
+      ciclo.cursos.forEach(curso => {
+        const li = document.createElement('li')
+        li.classList.add('list-group-item')
+        if (estado === 'Asignado') {
+          li.innerHTML = `
+          <div class="row text-center">
+            <div class="col-1">${ciclo.codigo}</div>
+            <div class="col-1">${curso.codigo}</div>
+            <div class="col-4 text-start">${curso.descripcion}</div>
+            <div class="col-6 text-start">
+              <button id="btnBusqueda" class="btn btn-success w-10">${estado}</button>
+            </div>
+          </div>
+        `
+        } else if (estado === 'Asignar') { 
+          li.innerHTML = `
+          <div class="row text-center">
+            <div class="col-1">${ciclo.codigo}</div>
+            <div class="col-1">${curso.codigo}</div>
+            <div class="col-4 text-start">${curso.descripcion}</div>
+            <div class="col-6 text-start">
+              <button data-id="${curso.idCurso}" class="btn btn-danger btn-asignar w-10">${estado}</button>
+            </div>
+          </div>
+        `
+        }
+      select.appendChild(li)
+    })
+  })
+  } catch (err) {
+    console.error(err)
+  }
+}
