4175807c4fed3a853cd65e36395bf6cb8849d8dd
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-13 22:18:06 -0600

    separación de cns

--- FE-asistencia/js/consultas.js
+++ FE-asistencia/js/consultas.js
@@ -1,10 +1,7 @@
-import { fetchAPI } from './api.js'
+import { cargarFacultades, cargarCiclos, cargarCursos } from './ui.js'
 
-export async function obtenerCursos(facultad, ciclo) {
-  let url = `/consultas/cursos?`
-
-  if (facultad) url += `facultad=${facultad}&`
-  if (ciclo) url += `ciclo=${ciclo}`
-
-  return await fetchAPI(url)
-}
+window.addEventListener('DOMContentLoaded', () => {
+  cargarFacultades()
+  document.getElementById('facultadSelect').addEventListener('change', cargarCiclos)
+  document.getElementById('botonBusca').addEventListener('click', cargarCursos)
+})
