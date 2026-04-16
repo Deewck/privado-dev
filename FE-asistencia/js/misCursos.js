3dc3f40b17d423b7e31004eea05437b9872a22ab
Dereck Lopez <dlopezr29@miumg.edu.gt>
2026-04-15 20:29:30 -0600

    mis Cursos FE

--- FE-asistencia/js/misCursos.js
+++ FE-asistencia/js/misCursos.js
@@ -1,9 +1,10 @@
-import { asignadosSN, misCursosEstudiante, cargarFacultades} from './ui.js'
+import { asignadosSN, misCursosEstudiante, cargarFacultades, asignaCursos} from './ui.js'
 cargarFacultades()
 asignadosSN()
 document.getElementById('btnBusqueda').addEventListener('click', misCursosEstudiante)
 document.getElementById('listaMisCursos').addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-asignar')) {
-    const idCurso = e.target.dataset.id
+    const codigoCurso = e.target.dataset.id
+    asignaCursos(codigoCurso)
   }
 })
